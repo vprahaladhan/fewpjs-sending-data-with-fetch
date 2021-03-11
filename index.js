507b0a563f53e7b8cb1d7677089efff4d09511c9
prahal <vprahaladhan@gmail.com>
2021-03-11 16:00:53 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+const submitData = (name, email) => {
+  fetch('http://localhost:3000/users', {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      name,
+      email
+    })  
+  })
+  .then(response => response.json())
+  .then(user => document.body.innerHTML = user.id)
+  .catch(error => document.body.innerHTML = error.message);
+}
+
+// submitData('Jim', 'Jim@jim.com');
